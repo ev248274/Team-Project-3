741ec5aec76bb8c4c3a0733b5b968684a0ec2484
joes574 <js260292@my.stchas.edu>
2024-04-16 23:34:07 -0500

    Finished functions create_binary_tree() and create_node()
    Initialized create_binary_tree() function, which builds the Morse code tree, setting binary_tree_root as it's root. Also created helper function create_node() for create_binary_tree() to help in it's implementation.

--- Team-Project-3/Main.cpp
+++ Team-Project-3/Main.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 int main() {
 	Morse_Code first;
-	first.create_binary_tree();
-
 
 	system("pause");
 	return 0;
--- Team-Project-3/Morse_Code.cpp
+++ Team-Project-3/Morse_Code.cpp
@@ -1,7 +1,7 @@
 #include "Morse_Code.h"
 
 // Default Constructor
-Morse_Code::Morse_Code() : binary_tree_root({}) {}
+Morse_Code::Morse_Code() : binary_tree_root(new Node("", "")) { create_binary_tree(); }
 
 // Destructor
 Morse_Code::~Morse_Code() { clear(binary_tree_root); }
@@ -24,7 +24,9 @@ Converts a string of letters into a string of codes
 @return: string of codes
 */
 std::string Morse_Code::encode(std::string& letters) {
-	// FIXME <-----------------------------------------------------
+	std::string codes = "";
+	// FIXME <-------------------------------------------------------------------------------------------
+	return codes;
 }
 
 /*
@@ -33,7 +35,9 @@ Converts a string of codes into a string of letters
 @return: string of letters
 */
 std::string Morse_Code::decode(std::string& codes) {
-	// FIXME <-----------------------------------------------------
+	std::string letters = "";
+	// FIXME <-------------------------------------------------------------------------------------------
+	return letters;
 }
 
 /*
@@ -42,5 +46,47 @@ Creates binary tree from text file
 void Morse_Code::create_binary_tree() {
 	std::string letter;
 	std::string code;
+	std::string original_code;
 	std::ifstream input_file;
+
+	input_file.open("Morse_Code.txt");
+
+	for (int i = 0; i < 26; ++i) {
+		getline(input_file, original_code);
+		letter = original_code.at(0);
+		original_code.erase(0, 1);
+		code = original_code;
+		create_node(binary_tree_root, code, original_code, letter);
+	}
+}
+
+/*
+Attaches an individual node to be put into the binary tree
+@param node: Pointer to node
+@param cde: Address of string morse code (to be changed)
+@param o_code: Address of string original code, which is the code without any changes
+@param ltr: Address of string letter
+*/
+void Morse_Code::create_node(Node* node, std::string& cde, std::string& o_code, std::string& ltr) {
+	if (cde == "") { 
+		node->letter = ltr;
+		node->code = o_code;
+		return;
+	}
+	else if (cde.at(0) == '.') {
+		if (!node->left) {
+			Node* node_l = new Node("", "");
+			node->left = node_l;
+			create_node(node_l, cde.erase(0, 1), o_code, ltr);
+		}
+		else { create_node(node->left, cde.erase(0, 1), o_code, ltr); }
+	}
+	else {
+		if (!node->right) {
+			Node* node_r = new Node("", "");
+			node->right = node_r;
+			create_node(node_r, cde.erase(0, 1), o_code, ltr);
+		}
+		else { create_node(node->right, cde.erase(0, 1), o_code, ltr); }
+	}
 }
